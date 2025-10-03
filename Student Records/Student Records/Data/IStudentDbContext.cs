8e0755187ed77a93a36f5ed697569dd6a6ae2c2e
MgKyaw <mgkyaw20112@gmail.com>
2025-10-03 22:40:54 +0700

    feat: Add IStudentDbContext

---
+++ Student Records/Student Records/Data/IStudentDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Student_Records.Data
+{
+    public interface IStudentDbContext
+    {
+        DbSet<Student> Student { get; set; }
+
+        Task<int> SaveChangesAsync();
+    }
+}
