45e19600511f544ebc8c05c21729c9423490918e
MgKyaw <mgkyaw20112@gmail.com>
2025-10-03 22:39:09 +0700

    feat: Add Student.cs

---
+++ Student Records/Student Records/Data/Student.cs
@@ -0,0 +1,13 @@
+﻿namespace Student_Records.Data
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string StudentName { get; set; }
+
+        public int Age { get; set; }
+        public string CourseTitle { get; set; }
+    }
+}
