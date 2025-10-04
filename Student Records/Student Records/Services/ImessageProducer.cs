3c4e48843151dc398cf54d41c30793d0e8d5bba3
MgKyaw <mgkyaw20112@gmail.com>
2025-10-04 19:09:21 +0700

    fix: Update IMessageProducer as an Interface

--- Student Records/Student Records/Services/ImessageProducer.cs
+++ Student Records/Student Records/Services/ImessageProducer.cs
@@ -1,25 +1,7 @@
-﻿using Newtonsoft.Json;
-using RabbitMQ.Client;
-using System.Text;
+﻿namespace Student_Records.Services;
 
-namespace Student_Records.Services;
-
-public class IMessageProducer
+public interface IMessageProducer
 {
-    public void SendMessage<T>(T message)
-    {
-        var factory = new ConnectionFactory
-        {
-            HostName = "localhost"
-        };
-        var connection = factory.CreateConnection();
-        using var channel = connection.CreateModel();
-        channel.QueueDeclare("students", exclusive: false);
-
-        var json = JsonConvert.SerializeObject(message);
-        var body = Encoding.UTF8.GetBytes(json);
-
-        channel.BasicPublish(exchange: "", routingKey: "students", body: body);
-    }
+    public void SendMessage<T>(T message);
 
 }
